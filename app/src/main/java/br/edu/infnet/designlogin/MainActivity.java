11b5ccda08b0caa98c85b265e6d36f3d28a021d9
hallpaz <hallpaaz@gmail.com>
2018-05-10 15:49:31 -0300

    First login screen completed (class 5).

---
+++ app/src/main/java/br/edu/infnet/designlogin/MainActivity.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.designlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.incremental_login);
+    }
+}
